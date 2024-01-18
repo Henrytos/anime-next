32f31377b7976b23230243634648062ec6020e01
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-01-17 21:16:17 -0300

    Fix typo in className value

--- src/components/constainer.tsx
+++ src/components/constainer.tsx
@@ -1,7 +1,7 @@
 export function Container({ ...props }) {
   return (
     <main
-      className="w-full h-[calc(100vh_-_7.5rem)] overflow-auto relative px-4 bg-neutral-900"
+      className="w-full h-[calc(100svh_-_7.5rem)] overflow-auto relative px-4 bg-neutral-900"
       {...props}
     />
   );
