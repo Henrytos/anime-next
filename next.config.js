121137bd058851e043b5ea16eb22360aa8f1cefd
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-01-27 19:40:25 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "anime-next-ruby.vercel.app",
+        hostname: "cdn.myanimelist.net",
         port: "",
-        pathname: "/_next/**",
+        pathname: "/images/**",
       },
     ],
   },
