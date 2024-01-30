6671f74ed4b50e0c99561b10010047dd26b35702
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-01-30 13:09:34 -0300

    Add Cypress Tests workflow and update e2e test files

--- cypress/e2e/add-to-favorite.cy.ts
+++ cypress/e2e/add-to-favorite.cy.ts
@@ -8,24 +8,35 @@ describe("template spec", () => {
 
   it("add-anime-to-favorite-to-search", () => {
     cy.visit("/");
+
     cy.searchByQuery("sousou no friren");
+
     cy.get("a[href^='/anime/']").first().click();
+
     cy.addAnimeToFavorite();
+
     cy.location("pathname").should("include", "/anime");
   });
 
   it("add-manga-to-favorite-to-search", () => {
     cy.visit("/");
+
     cy.searchByQuery("sousou no friren");
+
     cy.get("a[href^='/manga/']").first().click();
+
     cy.addAnimeToFavorite();
+
     cy.location("pathname").should("include", "/manga");
   });
 
   it("add-manga-to-favorite", () => {
     cy.visit("/manga");
+
     cy.get("a[href^='/manga/']").first().click();
+
     cy.addAnimeToFavorite();
+
     cy.location("pathname").should("include", "/manga");
   });
 });
--- cypress/e2e/search-in-home.cy.ts
+++ cypress/e2e/search-in-home.cy.ts
@@ -1,15 +1,19 @@
 describe("Search-in-home", () => {
   it("seacrh-anime-in-home", () => {
     cy.visit("/");
+
     cy.searchByQuery("sousou no frieren");
+
     cy.location("pathname").should("include", "/query");
   });
 
   it("seacrh-anime-in-url", () => {
     cy.visit("/query?q=");
+
     cy.on("uncaught:exception", () => {
       return false;
     });
+
     cy.location("pathname").should("equal", "/");
   });
 });
